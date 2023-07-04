42b5d04a68be8e94ec6b79f151c2beaa995f1c30
Yordanov on windows10 <theface11311@gmail.com>
2023-07-04 21:48:20 +0300

    ex

--- Encapsulation_ex4/01_wild_cat_zoo/project/zoo.py
+++ Encapsulation_ex4/01_wild_cat_zoo/project/zoo.py
@@ -1,63 +1,59 @@
+from typing import List
+
 from project.animal import Animal
+from project.worker import Worker
+
 
 class Zoo:
-    def __init__(self, name: str, budget: int, animal_capacity: int, worker_capacity: int):
-        self.name = name
-        self.__budget = budget
-        self.__animal_capacity = animal_capacity
-        self.__worker_capacity = worker_capacity
-        self.animals = []
-        self.workers = []
+    def __init__(self, name: str, budget: int, animal_capacity: int, workers_capacity: int):
+        self.name: str = name
+        self.__budget: int = budget
+        self.__animal_capacity: int = animal_capacity
+        self.__workers_capacity: int = workers_capacity
+        self.animals: List[Animal] = []
+        self.workers: List[Worker] = []
 
-    def add_animal(self, animal, price):
-        is_capacity_left = self.__animal_capacity > len(self.animals)
+    def add_animal(self, animal: Animal, price: int) -> str:
         is_budget = self.__budget >= price
-        if is_capacity_left and is_budget:
+        is_free_space = self.__animal_capacity > len(self.animals)
+
+        if is_budget and is_free_space:
             self.animals.append(animal)
             self.__budget -= price
             return f"{animal.name} the {animal.__class__.__name__} added to the zoo"
 
-        elif is_capacity_left and not is_budget:
+        elif is_free_space and not is_budget:
             return "Not enough budget"
 
         return "Not enough space for animal"
 
-    def hire_worker(self, worker):
-        if self.__worker_capacity > len(self.workers):
+    def hire_worker(self, worker: Worker) -> str:
+        if self.__workers_capacity > len(self.workers):
             self.workers.append(worker)
             return f"{worker.name} the {worker.__class__.__name__} hired successfully"
 
         return "Not enough space for worker"
 
-    def fire_worker(self, worker_name):
+    def fire_worker(self, worker_name:str) -> str:
+        this_worker = [w for w in self.workers if w.name == worker_name][0]
         try:
-            worker = [w for w in self.workers if worker_name == w.name][0]
-            self.workers.remove(worker)
-
-            return f"{worker_name} fired successfully"
-        except IndexError:
-            return f"There is no {worker_name} in the zoo"
+            self.workers.remove(this_worker)
+            return f"{this_worker.name} fired successfully"
+        except ValueError:
+            return  f"There is no {worker_name} in the zoo"
 
     def pay_workers(self):
-        needed_amount = sum([w.salary for w in self.workers])
-        if self.__budget >= needed_amount:
-            self.__budget -= needed_amount
+        payslips = sum([pay.salary for pay in self.workers])
+        if payslips <= self.__budget:
+            self.__budget -= payslips
             return f"You payed your workers. They are happy. Budget left: {self.__budget}"
 
         return "You have no budget to pay your workers. They are unhappy"
 
-    def tend_animals(self):
-        needed_amount = sum([a.money_for_care for a in self.animals])
-        if self.__budget >= needed_amount:
-            self.__budget -= needed_amount
-            return f"You tended all the animals. They are happy. Budget left: {self.__budget}"
-
-        return "You have no budget to tend the animals. They are unhappy."
-
-    def profit(self, amount):
+    def profit_amount(self, amount):
         self.__budget += amount
 
-    def animals_status(self):
+    def animal_status(self):
         result = f"You have {len(self.animals)} animals\n"
         lions = [a for a in self.animals if a.__class__.__name__ == "Lion"]
         tigers = [a for a in self.animals if a.__class__.__name__ == "Tiger"]
@@ -73,31 +69,10 @@ class Zoo:
         for t in tigers:
             result += f"{t}\n"
 
-        result += f"----- {len(cheetahs)} Cheetahs:\n"
+        result += f"----- {len(cheetahs)} Cheetahs:"
 
         for ch in cheetahs:
             result += f"{ch}\n"
 
         return result[:-1]
 
-    def workers_status(self):
-
-        result = f"You have {len(self.workers)} workers\n"
-
-        keepers = [w for w in self.workers if w.__class__.__name__ == "Keeper"]
-        caretakers = [w for w in self.workers if w.__class__.__name__ == "Caretaker"]
-        vets = [w for w in self.workers if w.__class__.__name__ == "Vet"]
-
-        result += f"----- {len(keepers)} Keepers:\n"
-        for k in keepers:
-            result += f"{k}\n"
-
-        result += f"----- {len(caretakers)} Caretakers:\n"
-        for c in caretakers:
-            result += f"{c}\n"
-
-        result += f"----- {len(vets)} Vets:\n"
-        for v in vets:
-            result += f"{v}\n"
-
-        return result[:-1]
--- test.py
+++ test.py
@@ -1,22 +1,5 @@
-class Animal:
-    name = ""
+first = 0
+second = 0
 
-    def eat(self):
-        print("I can eat")
-
-
-# inherit from Animal
-class Dog(Animal):
-
-    # override eat() method
-    def eat(self):
-        # call the eat() method of the superclass using super()
-        super().eat()
-
-        print("I like to eat bones")
-
-
-# create an object of the subclass
-labrador = Dog()
-
-labrador.eat()
+is_anything = first >= second
+print(is_anything)
